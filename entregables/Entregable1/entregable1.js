4cf35d594321ecb20a8b30364edf40e1ba674e40
ExpoJuy <crystaldesarrollo@gmail.com>
2023-01-29 11:44:58 -0300

    Commit con el primer desafío Clases con ECMAScript y ECMAScript avanzado terminado

--- entregables/Entregable1/entregable1.js
+++ entregables/Entregable1/entregable1.js
@@ -2,7 +2,6 @@
 let products = [];
 
 class ProductManager {
-  id;
   title;
   description;
   price;
@@ -10,8 +9,7 @@ class ProductManager {
   code;
   stock;
 
-  constructor(id, title, description, price, thumbnail, code, stock) {
-    this.id = id;
+  constructor(title, description, price, thumbnail, code, stock) {
     this.title = title;
     this.description = description;
     this.price = price;
@@ -22,19 +20,81 @@ class ProductManager {
   }
 
   addProduct() {
-    console.log(this.id);
-    console.log(this.title);
+    let codigo = products.find((product) => product.code == this.code);
+    if (codigo == undefined) {
+      products.push({
+        id: products.length + 1,
+        title: this.title,
+        description: this.description,
+        price: this.price,
+        thumbnail: this.thumbnail,
+        code: this.code,
+        stock: this.stock,
+      });
+      console.log(
+        "El producto con código: " +
+          this.code +
+          " se ha cargado satisfactoriamente"
+      );
+    } else {
+      console.log(
+        "El código de producto que desea ingresar ya se encuentra cargado"
+      );
+    }
+  }
 
-    products.push([this.id, this.title]);
+  getProducts() {
+    console.log("Los productos cargados son: ");
     console.log(products);
+    return products;
+  }
+
+  getProductById(iden) {
+    let id = products.find((product) => product.id == iden);
+    if (id == undefined) {
+      console.log("El producto que está buscando no se encuentra cargado");
+    } else {
+      console.log("Hemos encontrado el producto que estaba buscando: ");
+      console.log(id);
+      return id;
+    }
   }
 }
 
-function main() {
-  id = products.length + 1;
-  let title = "Sistema de ventas";
-  const p = new ProductManager(id, title);
-  p.addProduct();
-}
-
+//Ejecutamos el sistema principal
 main();
+
+function main() {
+  products = [];
+  // Primero hacemos un get product que me muestre mi array vacío.
+  const obtenerProducto = new ProductManager();
+  obtenerProducto.getProducts();
+
+  //Cargando producto 1
+  const p1 = new ProductManager(
+    (title = "Producto prueba"),
+    (description = "Este es un producto prueba 1"),
+    (price = 200),
+    (thumbnail = "Sin imagen"),
+    (code = "abc123"),
+    (stock = 25)
+  );
+  p1.addProduct();
+
+  //Mostramos mi primer producto cargado
+  obtenerProducto.getProducts();
+
+  //Cargando producto 2 que no se debería cargar y avisar que el producto con dicho código ya se encuentra cargado
+  const p2 = new ProductManager(
+    (title = "Producto prueba"),
+    (description = "Este es un producto prueba"),
+    (price = 200),
+    (thumbnail = "Sin imagen"),
+    (code = "abc123"),
+    (stock = 25)
+  );
+  p2.addProduct();
+
+  // Ahora vamos a buscar un producto por ID. Si existe devuelve el producto, sino muestra un mensaje de error
+  const pId1 = new ProductManager().getProductById(2);
+}
